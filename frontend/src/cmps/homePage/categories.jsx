4a0a77ceeba03cb8574024320ebb9abc7849d2e5
Itai Nissan <itainissan@gmail.com>
2023-07-20 15:23:31 +0300

    add caregories img+scss

--- frontend/src/cmps/homePage/categories.jsx
+++ frontend/src/cmps/homePage/categories.jsx
@@ -5,6 +5,9 @@ import { Button } from '@mui/material'
 import { setFilterBy } from '../../store/actions/foodActions'
 
 import backImg from '../../assets/images/background/cutting-board-2680168_edit3.jpg'
+import cheese from '../../assets/images/background/healthiest-cheese-1296x728-swiss.jpg'
+import soup from '../../assets/images/background/Homemade-Tomato-Soup-1.jpg'
+import fried from '../../assets/images/background/How-to-Use-A-Wok-For-Stir-Frying-Steaming.jpg'
 
 export const Categories = (props) => {
     window.scrollTo(0, 0)
@@ -20,22 +23,32 @@ export const Categories = (props) => {
 
     return (
         <section className="categories">
-            <h4>Explore by tags</h4>
+            {/* <h4>Explore by tags</h4> */}
             <div className="back-img">
                 <img src={backImg} alt="" />
             </div>
             <div className="explore">
                 <div className="explore-container">
-                    <h4>Explore new recipes</h4>
-                    <Link onClick={() => { setCategory('cheese') }} to="/ExplorRecipes">
-                        Cheese
-                    </Link>
-                    <Link onClick={() => { setCategory('Soup') }} to="/ExplorRecipes">
-                        Soup
-                    </Link>
-                    <Link onClick={() => { setCategory('fried') }} to="/ExplorRecipes">
-                        Fried
-                    </Link>
+                    <div className="categories-links">
+                        <Link onClick={() => { setCategory('cheese') }} to="/ExplorRecipes">
+                            <div className="category-link">
+                                <h4>Cheese</h4>
+                                <img src={cheese} alt="" />
+                            </div>
+                        </Link>
+                        <Link onClick={() => { setCategory('Soup') }} to="/ExplorRecipes">
+                            <div className="category-link">
+                                <h4>Soup</h4>
+                                <img src={soup} alt="" />
+                            </div>
+                        </Link>
+                        <Link onClick={() => { setCategory('fried') }} to="/ExplorRecipes">
+                            <div className="category-link">
+                                <h4>Fried</h4>
+                                <img src={fried} alt="" />
+                            </div>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </section>
