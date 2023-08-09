b551df309718b918791a47e243c3cb312d2846f0
Itai Nissan <itainissan@gmail.com>
2023-08-09 17:23:50 +0300

    db url to env

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,10 +1,5 @@
-module.exports = {
-  dbURL:
-    // 'mongodb+srv://itainis:<1UhCC6f9QbKHiQTo>@quiderrdb.uj0pssg.mongodb.net',
-    'mongodb+srv://itainis:1UhCC6f9QbKHiQTo@quiderrdb.uj0pssg.mongodb.net/?retryWrites=true&w=majority',
-}
+require('dotenv').config()
 
-// module.exports = {
-//   dbURL:
-//     'mongodb+srv://hadas:1234@cluster0.dud7tfv.mongodb.net',
-// }
+module.exports = {
+  dbURL: process.env.DB_URL
+}
