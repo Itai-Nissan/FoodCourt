c9cc66eb00cc05357cb480e1952a254b670abe8b
Itai Nissan <itainissan@gmail.com>
2023-08-22 21:14:35 +0300

    add step input validation

--- frontend/src/cmps/addRecipe/AddStep.jsx
+++ frontend/src/cmps/addRecipe/AddStep.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { setAddUserRecipe } from '../../store/actions/foodActions'
-
+import { utils } from '../../services/utils'
 import { Input } from '@mui/material'
 import { Button } from '@mui/material'
 
@@ -12,6 +10,19 @@ export const AddStep = (props) => {
     const [recipeStep, setStep] = useState('')
     const [updatedRecipeStep, setUpdatedStep] = useState('')
 
+    const [incorrectRecipeStep, setIncorrectRecipeStep] = useState(false)
+    const [incorrectRecipeStepText, setIncorrectRecipeStepText] = useState('')
+
+    function validatRecipeStep(value) {
+        console.log(value);
+        const validateReturn = utils.validatInput(value, 'Step')
+        setIncorrectRecipeStep(false)
+        if (validateReturn !== true) {
+            setIncorrectRecipeStep(true)
+            setIncorrectRecipeStepText(validateReturn)
+            return false
+        }
+    }
 
     function removeStep(e, index) {
         if (props.recipeInstructions.length === 1) {
@@ -26,8 +37,10 @@ export const AddStep = (props) => {
     }
 
     function addStep(e) {
-        if (recipeStep === '') return
         if (e.key === "Enter" || e.type === 'click') {
+            const checkRecipeStep = validatRecipeStep(recipeStep)
+            if (checkRecipeStep === false) return
+
             let updatedInstructions = null
             if (props.recipeInstructions.length === 0) {
                 updatedInstructions = props.recipeInstructions
@@ -50,6 +63,8 @@ export const AddStep = (props) => {
     function updateStep(event, index) {
         let updatedInstructions = props.recipeInstructions
         updatedInstructions.splice(index, 1, { display_text: event.target.value })
+        const checkRecipeStep = validatRecipeStep(event.target.value)
+        if (checkRecipeStep === false) return
         props.setRecipeInstructions(updatedInstructions)
 
         const newCount = props.stepCount + 1
@@ -60,6 +75,7 @@ export const AddStep = (props) => {
         <div className='add-remove-step-section'>
             <h3>Steps</h3>
             <hr />
+            <p className='incorrect'>{incorrectRecipeStep ? incorrectRecipeStepText : ''}</p>
             {props.recipeInstructions ? props.recipeInstructions.map((step, index) => {
                 return <div className="add-remove-step" key={index}>
                     <Button onClick={event => removeStep(event, index)} key={index}>-</Button>
@@ -67,8 +83,6 @@ export const AddStep = (props) => {
                         value={step.display_text}
                         onChange={event => { setUpdatedStep(event.target.value); updateStep(event, index) }}
                     />
-
-                    {/* <h4>{props.recipeInstructions[0].display_text === null ? '' : props.recipeInstructions[index].display_text}</h4> */}
                 </div>
             }) : []
             }
