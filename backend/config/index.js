21798787cdd7bc52383982d08d695ca7c9aa5ee7
Itai Nissan <itainissan@gmail.com>
2023-09-20 15:15:09 +0300

    dbURL direct to index

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
+
 let config
 
 if (process.env.NODE_ENV === 'production') {
   console.log('prod');
-  config = require('./prod')
+  config = process.env.DB_URL
+  // config = require('./prod')
 } else {
   console.log('dev');
   config = require('./dev')
