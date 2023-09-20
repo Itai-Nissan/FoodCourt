7d7481cb4249ff1a3cb9eb0250aca7efb4bebc1d
Itai Nissan <itainissan@gmail.com>
2023-09-20 15:03:42 +0300

    on prod go to build

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '25mb' }));
 app.use(session)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'public')))
+  app.use(express.static(path.resolve(__dirname, 'build')))
 } else {
   const corsOptions = {
     origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
